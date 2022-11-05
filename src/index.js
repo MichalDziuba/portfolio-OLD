325cc46fe18e71720389a3b8e87ebe88cd8ebc8f
Michał Dziuba <michaldziuba.praca@gmail.com>
2022-11-05 22:56:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
-import { NotFound } from './components/notFound';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="*"element={<NotFound/>} />
-      </Routes>
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
 
