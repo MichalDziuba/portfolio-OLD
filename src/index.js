68a5b0f75218214b81550663fb2d553a39b6e7f5
Michał Dziuba <michaldziuba.praca@gmail.com>
2022-11-05 22:53:07 +0100

    update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="*"element={<NotFound/>} />
       </Routes>
-    </HashR>
+    </HashRouter>
   </React.StrictMode>
 );
 
