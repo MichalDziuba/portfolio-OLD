2551415945cb5aa431cf83278da7eb3c8df0712e
Michał <michaldziuba.praca@gmail.com>
2022-12-19 16:26:51 +0100

    Update items

--- src/data/portfolioItems.js
+++ src/data/portfolioItems.js
@@ -5,15 +5,15 @@ import phonebookImage from '../images/Phonebook.webp'
 export const portfolioItems = [
   {
     title: "ImageFinder",
-    tech: "React,Tailwind,RestAPI",
-    link: "https://michaldziuba.github.io/goit-react-hw-04-hooks-images/",
+    tech: "React,Tailwind,JS",
+    link: "https://michaldziuba.github.io/ImagesFinder/",
     preview: `${imagefinderImage}`,
     description: "WebSite for searching images with PixaBay API.",
-    code: "https://github.com/MichalDziuba/goit-react-hw-04-hooks-images",
+    code: "https://github.com/MichalDziuba/ImagesFinder",
   },
   {
     title: "MoviesFinder",
-    tech: "HTML,CSS,React,RestAPI",
+    tech: "HTML,CSS,React,JS",
     link: "https://michaldziuba.github.io/goit-react-hw-05-movies/",
     preview: `${moviefinderImage}`,
     description:
@@ -22,7 +22,7 @@ export const portfolioItems = [
   },
   {
     title: "BooksFinder",
-    tech: "HTML,CSS,React,RestAPI",
+    tech: "HTML,CSS,React,JS",
     link: "https://michaldziuba.github.io/bookfinder/",
     preview: `${bookfinderImage}`,
     description: "WebSite for searching ebooks with Project Gutenberg API.",
